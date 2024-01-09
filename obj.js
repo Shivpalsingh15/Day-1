cb3c1d1efa489e5530dcbdeab634bfe64dd95bde
Shivpalsingh15 <singhshivpal.coder@gmail.com>
2024-01-09 19:39:29 +0530

    tailwind

---
+++ obj.js
@@ -0,0 +1,14 @@
+let obj ={
+    name: 'harry',
+    class: 10,
+    address: {
+        state: "Mp",
+        city: "Indore"
+    }
+}
+
+// console.log(address { state });
+const {
+    name
+}= obj
+console.log('state:', name)
